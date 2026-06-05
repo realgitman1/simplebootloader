b5d909ec6c8ccf4974d77de0093d125b8af7d318
realgitman1 <chococookieman1@gmail.com>
2026-06-05 15:43:44 +0900

    first boot

---
+++ my_boot.asm
@@ -0,0 +1,18 @@
+BITS 16
+org 0x7c00
+
+start :
+	mov si, 0
+
+print:
+	mov ah, 0x0e
+	mov al, [hello + si]
+	int 0x10
+	add si, 1
+	cmp byte [hello + si], 0
+	jne print
+hello:
+	db "Hello World!", 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
